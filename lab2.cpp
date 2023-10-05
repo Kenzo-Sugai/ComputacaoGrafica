80b97439240f605440a9c4f3c4966a528fa2d0a5
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-10-04 22:38:47 -0400

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,170 +1,227 @@
+#include <GL/freeglut.h>
+#include <iostream>
 
-#include <GL/freeglut.h> 
-#include <stdlib.h>
-#include <stdio.h>
+#define LAR 400
+#define ALT 400
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
+#define MAIORX 200.0f
+#define MENORX -200.0f
+#define MAIORY 200.0f
+#define MENORY -200.0f
 
-#define LAR 400 // largula da sua janela Windows
-#define ALT 400 // altura da sua janela Windows
+#define POSWX 250
+#define POSWY 150
 
-#define dimTx 256
+#define tang1x 10.0f
+#define tang1y -145.0f
+#define tang2x 10.0f
+#define tang2y -185.0f
 
-using namespace std;
-using namespace cv;
+GLfloat B[4][2];
 
-// Matriz RGB para guardar a imagem de textura
-uchar imTex[dimTx][dimTx][4];
+GLfloat P[4][4];
 
-// variáveis para o ângulo de perspectiva e para o aspecto da projeção
-// são usadas na função gluPerspective
-//A variavel rangle é alterada pelo teclado se vc clicar em 'a', já a variável fAspect é alterada altomaticamente
-//sempre que vc muda o aspecto da janela
-GLfloat rangle, fAspect;
+GLboolean GET_POINTS = 0;
 
+GLint TOTAL_POINTS = 0;
 
-// variáveis que controlam o clique do mouse para operações geométricas
-// são atualizadas nas funções de callback de mouse (GerenciaMouse) e
-// operações geométricas como glTranslate
+GLfloat bx, by;
+
+GLfloat angle, fAspect, rot = 0;
+
+GLfloat angleX = 0, angleY = 0;
+
 GLfloat dx = 0, dy = 0, dz = 0;
 
-// essas variáveis aqui são controladas pela Callback de mouse
-// servem para converter as coordenadas do Windows em coordenadas do OpenGL
-GLfloat cx = 0, cy = 0;
+void myMousefuncBezierHermitInterate(int button, int state, int x, int y) {
 
-void carregaTextura() {
+	switch (button) {
+	case GLUT_LEFT_BUTTON: {
+		bx = x - (LAR / 2);
+		by = (ALT / 2) - y;
+		GET_POINTS = 1;
+		break;
+	}
+	case GLUT_RIGHT_BUTTON: {
+		GET_POINTS = 0;
+		TOTAL_POINTS = 0;
+		break;
+	}
+	case GLUT_MIDDLE_BUTTON: {
+		break;
+	}
+	}
 
-	Mat imTexture, imTexture256;
+	std::cout << bx << " " << by << std::endl;
 
-	imTexture = imread("img24.jpg", 1);
+	glutPostRedisplay();
 
-	resize(imTexture, imTexture256, Size(256, 256));
+}
 
-	namedWindow("Teste Textura 256 x 256", 2);
+void DesenhaHermiteGrau3() {
 
-	imshow("Teste Textura 256 x 256", imTexture256);
+	GLfloat H[4][4] = {
+		{2.0f, -2.0f, 1.0f, 1.0f},
+		{-3.0f, 3.0f, -2.0f, -1.0f},
+		{0.0f, 0.0f, 1.0f, 0.0f},
+		{1.0f, 0.0f, 0.0f, 0.0f}
+	};
 
-	for (int y = 0; y < imTexture256.rows; y++) {
+	if ((TOTAL_POINTS == 0) && (GET_POINTS)) {
+		P[0][0] = bx;
+		P[0][1] = by;
+		P[0][2] = tang1x;
+		P[0][3] = tang1y;
+		TOTAL_POINTS += 1;
+	}
+	if ((TOTAL_POINTS > 0) && (GET_POINTS) && (TOTAL_POINTS < 2)) {
 
-		for (int x = 0; x < imTexture256.cols; x++) {
-
-			Vec3b cor = imTexture256.at<Vec3b>(y, x);
-
-			imTex[y][x][0] = 255 - cor[2]; // blue
-			imTex[y][x][1] = 255 - cor[1]; // green
-			imTex[y][x][2] = 255 - cor[0]; // red
-
+		if ((P[TOTAL_POINTS - 1][0] != bx) && (P[TOTAL_POINTS - 1][1] != by)) {
+			P[TOTAL_POINTS][0] = bx;
+			P[TOTAL_POINTS][1] = by;
+			P[TOTAL_POINTS][2] = tang2x;
+			P[TOTAL_POINTS][3] = tang2y;
+			TOTAL_POINTS += 1;
+			if (TOTAL_POINTS > 2) TOTAL_POINTS = 2;
 		}
 	}
 
-	imTexture256.release(); // desaloca imagem
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glPointSize(5.0f);
+	for (int i = 0; i < TOTAL_POINTS; i++) {
+		glBegin(GL_POINTS);
+		glVertex2f(P[i][0], P[i][1]);
+		glEnd();
+	}
+
+	GLfloat T[4];
+	GLfloat M[4];
+
+	if (TOTAL_POINTS == 2) {
+		GLfloat xini, yini, xfin, yfin;
+
+		float t = 0.0;
+		float passo = 0.01;
+
+		xini = P[0][0] * (2 * pow(t, 3) - 3 * pow(t, 2) + 1) + P[1][0] * (-2 * pow(t, 3) + 3 * pow(t, 2)) + P[0][2] * (pow(t, 3) - 2 * pow(t, 2) + t) + P[1][2] * (pow(t, 3) - pow(t, 2));
+		yini = P[0][1] * (2 * pow(t, 3) - 3 * pow(t, 2) + 1) + P[1][1] * (-2 * pow(t, 3) + 3 * pow(t, 2)) + P[0][3] * (pow(t, 3) - 2 * pow(t, 2) + t) + P[1][3] * (pow(t, 3) - pow(t, 2));
+
+		for (float t = 0.0 + passo; t <= 1.0; t += passo) {
+
+			xfin = P[0][0] * (2 * pow(t, 3) - 3 * pow(t, 2) + 1) + P[1][0] * (-2 * pow(t, 3) + 3 * pow(t, 2)) + P[0][2] * (pow(t, 3) - 2 * pow(t, 2) + t) + P[1][2] * (pow(t, 3) - pow(t, 2));
+			yfin = P[0][1] * (2 * pow(t, 3) - 3 * pow(t, 2) + 1) + P[1][1] * (-2 * pow(t, 3) + 3 * pow(t, 2)) + P[0][3] * (pow(t, 3) - 2 * pow(t, 2) + t) + P[1][3] * (pow(t, 3) - pow(t, 2));
+
+			glBegin(GL_LINE_STRIP);
+			glVertex2f(xini, yini);
+			glVertex2f(xfin, yfin);
+			glEnd();
+
+			xini = xfin;
+			yini = yfin;
+
+		}
+
+	}
+
+	std::cout << "TOTAL POINTS: " << TOTAL_POINTS << std::endl;
+
 }
 
-// função que desenha uma malha 3D
-void DesenhaMalha();
+void DesenhaBezierGrau3() {
 
+	GLfloat B[4][4] = {
+		{-1.0f, 3.0f, -3.0f, 1.0f},
+		{3.0f, -6.0f, 3.0f, 0.0f},
+		{-3.0f, 3.0f, 0.0f, 0.0f},
+		{1.0f, 0.0f, 0.0f, 0.0f}
+	};
 
-// Essa função, que não é callback, serve para configurar a iluminação
-// ela é chamada uma única vez na função de Incializa, chamada pela main()
-void ConfiguraIluminacao(void)
-{
-	GLfloat luzAmbiente[4] = { 0.2,0.2,0.2,1.0 }; // definição de luz ambiente
-	GLfloat luzDifusa[4] = { 0.7,0.7,0.7,1.0 };	   // definição de luz difusa
-	GLfloat luzEspecular[4] = { 1.0, 1.0, 1.0, 1.0 };// definição de luz especular (brilho) 
-	GLfloat posicaoLuz[4] = { 0.0, 50.0, 50.0, 1.0 }; // posição do spot de luz
+	if ((TOTAL_POINTS == 0) && (GET_POINTS)) {
+		P[0][0] = bx;
+		P[0][1] = by;
+		P[0][2] = tang1x;
+		P[0][3] = tang1y;
+		TOTAL_POINTS += 1;
+	}
+	if ((TOTAL_POINTS > 0) && (GET_POINTS) && (TOTAL_POINTS < 4)) {
 
-	// Capacidade de brilho do material
-	GLfloat especularidade[4] = { 1.0,1.0,1.0,1.0 };
-	GLint especMaterial = 60;
+		if ((P[TOTAL_POINTS - 1][0] != bx) && (P[TOTAL_POINTS - 1][1] != by)) {
+			P[TOTAL_POINTS][0] = bx;
+			P[TOTAL_POINTS][1] = by;
+			P[TOTAL_POINTS][2] = tang2x;
+			P[TOTAL_POINTS][3] = tang2y;
+			TOTAL_POINTS += 1;
+			if (TOTAL_POINTS > 4) TOTAL_POINTS = 4;
+		}
+	}
 
-	// Especifica que a cor de fundo da janela será preta
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glPointSize(5.0f);
+	for (int i = 0; i < TOTAL_POINTS; i++) {
+		glBegin(GL_POINTS);
+		glVertex2f(P[i][0], P[i][1]);
+		glEnd();
+	}
 
-	// Habilita o modelo de colorização de Gouraud
-	glShadeModel(GL_SMOOTH);
+	GLfloat T[4];
+	GLfloat M[4];
 
-	// Define a refletância do material 
-	glMaterialfv(GL_FRONT, GL_SPECULAR, especularidade);
-	// Define a concentração do brilho
-	glMateriali(GL_FRONT, GL_SHININESS, especMaterial);
+	if (TOTAL_POINTS == 4) {
+		GLfloat xini, yini, xfin, yfin;
 
-	// Ativa o uso da luz ambiente 
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
+		float t = 0.0;
+		float passo = 0.01;
 
-	// Define os parâmetros da luz de número 0
-	glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular);
-	glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
+		xini = P[0][0] * pow((1 - t), 3) + (3 * P[1][0] * pow(t, 1) * pow((1 - t), 2)) + (3 * P[0][2] * pow(t, 2) * pow((1 - t), 1)) + P[1][2] * pow(t, 3);
+		yini = P[0][1] * pow((1 - t), 3) + (3 * P[1][1] * pow(t, 1) * pow((1 - t), 2)) + (3 * P[0][3] * pow(t, 2) * pow((1 - t), 1)) + P[1][3] * pow(t, 3);
 
-	// Habilita a definição da cor do material a partir da cor corrente
-	glEnable(GL_COLOR_MATERIAL);
-	//Habilita o uso de iluminação
-	glEnable(GL_LIGHTING);
-	// Habilita a luz de número 0
-	glEnable(GL_LIGHT0);
-	// Habilita o depth-buffering
-	glEnable(GL_DEPTH_TEST);
+		for (float t = 0.0 + passo; t <= 1.0; t += passo) {
+
+			xfin = P[0][0] * pow((1 - t), 3) + (3 * P[1][0] * pow(t, 1) * pow((1 - t), 2)) + (3 * P[0][2] * pow(t, 2) * pow((1 - t), 1)) + P[1][2] * pow(t, 3);
+			yfin = P[0][1] * pow((1 - t), 3) + (3 * P[1][1] * pow(t, 1) * pow((1 - t), 2)) + (3 * P[0][3] * pow(t, 2) * pow((1 - t), 1)) + P[1][3] * pow(t, 3);
+
+			glBegin(GL_LINE_STRIP);
+			glVertex2f(xini, yini);
+			glVertex2f(xfin, yfin);
+			glEnd();
+
+			xini = xfin;
+			yini = yfin;
+
+		}
+
+	}
+
+	std::cout << "TOTAL POINTS: " << TOTAL_POINTS << std::endl;
+
 }
 
 
+void DesenhaCurvaHermiteBezier() {
+	glClear(GL_COLOR_BUFFER_BIT);
 
-// Função callback chamada para fazer o desenho
-void Desenha(void)
-{
-	// pinta a cor do fundo
+	//glPushMatrix();
+	//glClearColor(1.0, 1.0, 1.0, 1.0);
+	DesenhaHermiteGrau3();
+	//glPopMatrix();
+
+	//glLoadIdentity();
+
+	//glutSwapBuffers();
+	glFlush();
+}
+
+void DesenhaCurvasHermiteEBezier() {
+
+	// Limpa a janela de visualização com a cor  
+	// de fundo definida previamente
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// define a cor dos objetos que vão ser construídos
-	glColor3f(0.0f, 0.0f, 1.0f);
+	DesenhaBezierGrau3(); /// usar
 
-	// Desenha o teapot com a cor corrente (wire-frame)
-	glPushMatrix(); // empacota as ações
-	glTranslatef(dx, dy, dz); // essa função translada de dx, dy e dz tudo que vier abaixo dela e dentro da glPushMatrix-glPopMatrix
-	DesenhaMalha();
-	glPopMatrix();  // desempacota as ações
-
-	// Executa os comandos OpenGL
-	glutSwapBuffers();  // troca de buffer para acelerar a cena
+	glutSwapBuffers();
 }
 
-
-void DesenhaMalha()
-{
-	//glutWireTeapot(50.0f);     // constrói uma malha Wire 
-	//glutSolidTeapot(50.0f);     // constrói uma malha Solida 
-
-	// esta macro desenha um simples quadrado
-	glBegin(GL_QUADS);
-	glVertex3f(-64, -64, 0);
-	glVertex3f(64, -64, -50);
-	glVertex3f(64, 64, -50);
-	glVertex3f(-64, 64, 50);
-	glEnd();
-}
-
-
-
-// Inicializa parâmetros iniciais, é chamada na main()
-void Inicializa(void)
-{
-	// configura iluminação
-	ConfiguraIluminacao();
-
-	// define a cor de fundo
-	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-
-	carregaTextura();
-
-	// define o ângulo inicial de perspectiva, na função gluPerspective()
-	//Essa variável é alterada pelo teclado se vc clicar em 'a'
-	rangle = 45;
-}
-
-
-// Função usada para especificar o volume de visualização
-// se vc alterou algum parâmetro de visualização, essa funçào é chamada
 void EspecificaParametrosVisualizacao(void)
 {
 	// Especifica sistema de coordenadas de projeção
@@ -173,7 +230,7 @@ void EspecificaParametrosVisualizacao(void)
 	glLoadIdentity();
 
 	// Especifica a projeção perspectiva
-	gluPerspective(rangle, fAspect, 0.1, 500);
+	gluPerspective(angle, fAspect, 0.1, 500);
 
 	// Especifica sistema de coordenadas do modelo
 	glMatrixMode(GL_MODELVIEW);
@@ -184,112 +241,62 @@ void EspecificaParametrosVisualizacao(void)
 	gluLookAt(0, 0, 200, 0, 0, 0, 0, 1, 0);
 }
 
-
-// Função callback chamada quando o tamanho da janela é alterado
 void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 {
-	// Para previnir uma divisão por zero
-	if (h == 0) h = 1;
+	//GLsizei largura, altura;
+	GLfloat largura, altura;
 
-	// Especifica o tamanho da viewport
-	glViewport(0, 0, w, h);
+	// Evita a divisao por zero
+	if (h == 0)
+		h = 1;
 
-	// Calcula a correção de aspecto
-	fAspect = (GLfloat)w / (GLfloat)h;
+	// Atualiza as variáveis
+	largura = w;
+	altura = h;
 
-	// se vc alterou algum parâmetro de visualização, essa funçào é chamada
-	EspecificaParametrosVisualizacao();
+	// Especifica as dimensões da Viewport
+	glViewport(0, 0, largura, altura);
+
+	// Inicializa o sistema de coordenadas
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	// Estabelece a janela de seleção (esquerda, direita, inferior,
+	// superior) mantendo a proporção com a janela de visualização
+	if (largura <= altura)
+		gluOrtho2D(MENORX, MAIORX, MENORY * altura / largura, MAIORY * altura / largura);
+	//gluOrtho2D(MENORX_BEZIER, MAIORX_BEZIER, MENORY_BEZIER * altura / largura, MAIORY_BEZIER * altura / largura);
+	else
+		gluOrtho2D(MENORX * largura / altura, MAIORX * largura / altura, MENORY, MAIORY);
+	//gluOrtho2D(MENORX_BEZIER * largura / altura, MAIORX_BEZIER * largura / altura, MENORY_BEZIER, MAIORY_BEZIER);
 }
 
-
-// calback de teclado
-void Teclado(unsigned char key, int x, int y) {
-
-	switch (key) {
-
-	case 'r': dx = dx + 10; break;  //desloca para a direita
-
-	case 'l': dx = dx - 10; break;  //desloca para a esquerda
-
-	case 'a':                      // aumenta o ângulo de visualização da função gluPerspective(), provocando ZOO-IN
-		rangle = rangle + 5;
-		if (rangle > 360) rangle = 0;
-		break;
-
-	case 'b':                      // diminui o ângulo de visualização da função gluPerspective(), provocando ZOO-OUT
-		rangle = rangle - 5;
-		if (rangle < 0) rangle = 0;
-		break;
-
-	}
-
-	// se vc alterou algum parâmetro de visualização, essa funçào é chamada
-	EspecificaParametrosVisualizacao();
-
-	// dispara a callback de Desenha() para desenhar o que alterou!
-	glutPostRedisplay();
-}
-
-
-// Função callback chamada para gerenciar eventos do mouse
-void GerenciaMouse(int button, int state, int x, int y)
+void Inicializa(void)
 {
-	// aqui tranaforma o x do windows no cx do OpenGL
-	cx = x - (LAR / 2);
-
-	// aqui tranaforma o y invertido do windows no cy do OpenGL
-	cy = (ALT / 2) - y;
-
-	/*	if (button == GLUT_LEFT_BUTTON)
-		if (state == GLUT_DOWN) {
-		// mude uma variável controlada pelo mouse aqui
-	}
-
-	if (button == GLUT_RIGHT_BUTTON)
-		if (state == GLUT_DOWN) {
-		// mude uma variável controlada pelo mouse aqui
-		}
-*/
-// se vc alterou algum parâmetro de visualização, essa funçào é chamada
-	EspecificaParametrosVisualizacao();
-
-	// dispara a função callback de Desenho(), caso vc tenha alterado alguma coisa no visual
-	glutPostRedisplay();
+	// Define a cor de fundo da janela de visualização como branca
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
+int main(int argc, char* argv[]) {
 
-
-// Programa Principal
-int main(int argc, char* argv[])
-{
-	// essa função inicializa a glut
 	glutInit(&argc, argv);
 
-	// ess função inicializa o Display com um buffer RGB duplo
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
-	// essa função define as dimenções de sua janela Windows
+	glutInitWindowPosition(POSWX, POSWY);
+
 	glutInitWindowSize(LAR, ALT);
 
+	glutCreateWindow("Curvas");
 
-	//aqui vc cria uma Janela Windows com as dimenções definidas acima
-	glutCreateWindow("Visualizacao 3D");
+	glutDisplayFunc(DesenhaCurvasHermiteEBezier);
 
-	// aqui vc ativa uma callback de Desenho
-	glutDisplayFunc(Desenha);
-
-	// aqui vc ativa uma callBack para ajustar a janela de saída toda vez que vc alterar ela
 	glutReshapeFunc(AlteraTamanhoJanela);
 
-	// aqui vc ativa uma callbcak de mouse toda vez que vc clica nele
-	glutMouseFunc(GerenciaMouse);
+	glutMouseFunc(myMousefuncBezierHermitInterate);
 
-	// aqui vc ativa uma callback de teclado toda vez que vc clica nele
-	glutKeyboardFunc(Teclado);
-
-	// aqui vc inicializa qualquer coisa fixa (nào é callback)
 	Inicializa();
 
-	// quando tudo está pronto, aqui dispara tudo
 	glutMainLoop();
+
 }
